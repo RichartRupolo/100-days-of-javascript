aa6d7461e87829a592d8df162903dc2097f2eefd
DESKTOP-9TCSV3O\Richart <richart.rupolo@hotmail.com>
2023-03-14 21:57:53 -0300

    day 11

---
+++ 011 - quiz app/models/Question.js
@@ -0,0 +1,11 @@
+ class Question{
+    constructor(text, choices, answer){
+        this.text    = text
+        this.choices = choices
+        this.answer  = answer
+    }
+
+    isCorrectAnsware(choice){
+        return this.answer === choice
+    }
+}
